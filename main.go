8560f07b91362afe9ad6d97cd1a7376b3a415d01
fgitonga <mfredgitonga@gmail.com>
2025-03-06 18:07:44 +0300

    format:  amend names to standard - psswd o password - signup to sign-up

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 	http.HandleFunc("/signin", handlers.SigninHandler)
 	http.HandleFunc("/auth/google/signin", auth.GoogleSignIn)
 	http.HandleFunc("/auth/google/signin/callback", auth.GoogleSignInCallback)
-	http.HandleFunc("/auth/github/signup", auth.GithubSignUp)
+	http.HandleFunc("/auth/github/sign-up", auth.GithubSignUp)
 	http.HandleFunc("/auth/github/callback", auth.GithubCallback)
-	http.HandleFunc("/home", handlers.Homehandler)
+	http.HandleFunc("/home", handlers.HomeHandler)
 	fmt.Println("Server running at: http://localhost:9000")
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
