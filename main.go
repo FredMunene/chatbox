d82d85ef1c122b55431a786df0d3ea830e7e6fed
fgitonga <mfredgitonga@gmail.com>
2025-03-06 16:15:01 +0300

    update route for github callback, and loading environment variables

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ import (
 
 	"forum/backend/auth"
 	"forum/backend/handlers"
+	"forum/backend/utils"
 )
 
 func main() {
 	// load environment variables
 
+	utils.LoadEnvVariables(".env")
+
 	http.HandleFunc("/signin", handlers.SigninHandler)
 	http.HandleFunc("/auth/google/signin", auth.GoogleSignIn)
 	http.HandleFunc("/auth/google/signin/callback", auth.GoogleSignInCallback)
+	http.HandleFunc("/auth/github/signup", auth.GithubSignUp)
+	http.HandleFunc("/auth/github/callback", auth.GithubCallback)
 	http.HandleFunc("/home", handlers.Homehandler)
 	fmt.Println("Server running at: http://localhost:9000")
 	log.Fatal(http.ListenAndServe(":9000", nil))
