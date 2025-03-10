2470af0aa177cc17c1e400758b40e81ee1de5594
fgitonga <mfredgitonga@gmail.com>
2025-03-10 14:25:13 +0300

    add userAuthentication by querying bd against user-email - implement setting session cookie and generation of user   session token

--- backend/auth/auth.go
+++ backend/auth/auth.go
@@ -1,6 +1,15 @@
 package auth
 
 import (
+	"database/sql"
+	"encoding/base64"
+	"errors"
+	"log"
+	"net/http"
+
+	"forum/backend/util"
+
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -10,4 +19,69 @@ func ComparePassword(userPassword string, dbPassword string) error {
 		return err
 	}
 	return nil
+}
+
+func handleUserAuth(w http.ResponseWriter, email, username string) bool {
+	var userID int
+
+	err := util.Database.QueryRow(
+		"SELECT id from Users WHERE email = ?", email).Scan(&userID)
+
+	//  Create a new user if not found
+	if errors.Is(err, sql.ErrNoRows) {
+		res, err := util.Database.Exec(
+			"INSERT INTO users(username, email) VALUES(?,?)",
+			username, email,
+		)
+		if err != nil {
+			log.Printf("User creation failed: %v", err)
+			return false
+
+		}
+		id, _ := res.LastInsertId()
+		userID = int(id)
+	} else if err != nil {
+		log.Printf("Database error: %v", err)
+		return false
+	}
+
+	SetSessionCookie(w, userID)
+	return true
+}
+
+// SetSessionCookie sets a session cookie for the given user ID.
+func SetSessionCookie(w http.ResponseWriter, userID int) {
+	token := generateSessionToken()
+	_, err := util.Database.Exec(
+		"INSERT INTO tblSessions(user_id, session_token) VALUES(?, ?)",
+		userID, token,
+	)
+	if err != nil {
+		log.Printf("Session creation failed: %v", err)
+		return
+	}
+
+	// Set the session cookie for the user
+	http.SetCookie(w, &http.Cookie{
+		Name:     "forum_session",
+		Value:    token,
+		HttpOnly: true,
+		Path:     "/",
+		MaxAge:   86400, // 1 day
+	})
+}
+
+func generateSessionToken() string {
+	// Generate a new UUID (UUIDv4)
+	u := uuid.New()
+
+	// Convert the UUID to its byte representation
+	uBytes, err := u.MarshalBinary()
+	if err != nil {
+		log.Printf("Error marshaling UUID:", err)
+		return ""
+	}
+
+	// Encode the bytes in URL-safe base64 format
+	return base64.URLEncoding.EncodeToString(uBytes)
 }
