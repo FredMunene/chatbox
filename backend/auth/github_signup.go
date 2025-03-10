984e1e3985e3f4237952573f9932cef500953fe6
fgitonga <mfredgitonga@gmail.com>
2025-03-10 14:24:05 +0300

    add sign-in logic - allow query and saving of user info in db - allow session creation - enabling cors -

--- backend/auth/github_signup.go
+++ backend/auth/github_signup.go
@@ -6,8 +6,13 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
+	"forum/backend/repositories"
 	"forum/backend/utils"
+
+	"forum/backend/handlers"
+	"forum/backend/util"
 )
 
 const (
@@ -82,10 +87,44 @@ func GithubCallback(w http.ResponseWriter, r *http.Request) {
 	switch flowType {
 	case "signup":
 		// handle user sign up
-		http.Redirect(w, r, "/home?status=success", http.StatusSeeOther)
+		if handleUserAuth(w, user.Email, user.Login) {
+			log.Printf("User signup successful")
+			http.Redirect(w, r, "/sign-up?status=success", http.StatusTemporaryRedirect)
+		} else {
+
+			log.Printf("User signup failed")
+			http.Redirect(w, r, "/sign-up?status=auth_failed", http.StatusSeeOther)
+
+		}
 
 	case "signin":
 		// handle user sign in
+		var userID int
+
+		err := util.Database.QueryRow("SELECT id FROM Users WHERE email = ?", user.Email).scan(&userID)
+		if err != nil {
+			http.Redirect(w, r, "/sign-inp?error=no_account", http.StatusTemporaryRedirect)
+		}
+
+		sessionToken := handlers.CreateSession()
+		if userID != 0 {
+			handlers.DeleteSession(userID)
+		}
+
+		handlers.EnableCors(w)
+		handlers.SetSessionCookie(w,sessionToken)
+		handlers.SetSessionData()
+		handlers.SetSessionData()
+
+		expiryTime := time.Now().Add(24* time.Hour)
+
+		err = repositories.StoreSession(userID, sessionToken, expiryTime)
+		if err != nil {
+			log.Printf("Failed to store session token:%v", err)
+			http.Redirect(w,r,"/sign-in?error=session_error", http.StatusTemporaryRedirect)
+			return
+		}
+
 		http.Redirect(w, r, "/home?status=success", http.StatusSeeOther)
 
 	default:
